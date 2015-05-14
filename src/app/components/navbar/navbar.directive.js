5f0ce78e1ccf846fcfd4469566664d611a458872
kavare <aarontthsieh@gmail.com>
2015-05-14 15:37:58 +0900

    [navbar] adding scroll fixed directive event

---
+++ src/app/components/navbar/navbar.directive.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular
+  .module('neocareer')
+  .directive('navbarFixed', ['$window', function($window) {
+    var $win = angular.element($window); // wrap window object as jQuery object
+
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        var topClass = attrs.navbarFixed, // get CSS class from directive's attribute value
+            offsetTop = element.offset().top; // get element's offset top relative to document
+
+        $win.on('scroll', function (e) {
+          if ($win.scrollTop() > offsetTop) {
+            element.addClass(topClass);
+          } else {
+            element.removeClass(topClass);
+          }
+        });
+      }
+    };
+  }]);
